2ed5593b84baee4c0ebdb764dc2ee6e8c2cc2b35
narae.eo <narae.eo@lge.com>
2025-01-15 16:03:04 +0900

    Initial commit from Create Next App
    [desc] next

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,26 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function AcmeLogo() {
+  return (
+     <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+      <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+            <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
+      />
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p>
+    </div>
+  );
+}
